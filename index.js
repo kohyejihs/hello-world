e38887325b24b3e38fdd0ca96e80a8df41ba0781
kohyejihs <ko.hjha@gmail.com>
2018-09-11 15:44:00 +0900

    토마토2

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var express = require('express'); // 설치한 express module을 불러와서 변수(express)에 담습니다.
 var app = express(); //express를 실행하여 app object를 초기화 합니다.
 
-app.get('/',function (req,res) { // '/' 위치에 'get'요청을 받는 경우,
 app.use(express.static(__dirname + '/public')); // 1
 
 app.listen(3000, function(){ //3000번 포트를 사용합니다.
