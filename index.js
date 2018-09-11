91e09ab832a9fdf69aab364ca483b2661815deb0
kohyejihs <ko.hjha@gmail.com>
2018-09-11 15:52:17 +0900

    이름 입력받기

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-var express = require('express'); // 설치한 express module을 불러와서 변수(express)에 담습니다.
-var app = express(); //express를 실행하여 app object를 초기화 합니다.
+// index.js
 
-app.use(express.static(__dirname + '/public')); // 1
+﻿var express = require('express');
+var app = express();
 
-app.listen(3000, function(){ //3000번 포트를 사용합니다.
- console.log('Server On!'); //서버가 실행되면 콘솔창에 표시될 메세지입니다.
+app.set("view engine","ejs"); // 1
+app.use(express.static(__dirname + '/public'));
+
+// 2
+app.get("/hello", function(req,res){
+ res.render("hello", {name:req.query.nameQuery});
+}); 
+
+// 3
+app.get("/hello/:nameParam", function(req,res){ 
+ res.render("hello", {name:req.params.nameParam});
+});
+
+app.listen(3000, function(){
+ console.log('Server On!');
 });
